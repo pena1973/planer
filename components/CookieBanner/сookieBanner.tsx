7532bef32b69e240885c38c239dc2f9c9d0dd0fd
pena1973 <penapisemnet@gmail.com>
2025-07-07 16:46:14 +0300

    07072025 сборка2

--- components/CookieBanner/сookieBanner.tsx
+++ components/CookieBanner/сookieBanner.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import styles from './сookieBanner.module.scss'
+import styles from './cookieBanner.module.scss'
 import Link from 'next/link';
 
 export default function CookieBanner() {
